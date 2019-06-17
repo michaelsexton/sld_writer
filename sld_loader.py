763bed4322aca3b575fb4817b5fa52eb72f4c2c1
michaelsexton <michael.sexton@ga.gov.au>
2019-06-17 11:17:51 +1000

    Change URI

--- sld_loader.py
+++ sld_loader.py
@@ -1,10 +1,11 @@
 import glob, os
 from geoserver.catalog import Catalog
 os.chdir("sld")
-cat = Catalog("http://localhost:48080/minresource-minesatlas-gws/rest")
+cat = Catalog("http://localhost:28080/earthresource/rest")
 for file in glob.glob("*.sld"):
 
     with open(file) as f:
+        print file
         sld_name = os.path.splitext(file)[0]
         cat.create_style(sld_name, f.read(), overwrite=True)
         
